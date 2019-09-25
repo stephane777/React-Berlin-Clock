a52b4ea9062ac7f1a0b83e807a1984f22b3e5966
Stephane Candelas <stephanecandelas@gmail.com>
2019-09-26 00:19:12 +0100

    add babel preset

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+	return <div>toto a ski but</div>;
+};
+ReactDOM.render(<App />, document.getElementById("root"));
