531d44eabb8fc6a536887ef4dc445c7eda22c616
Stephane Candelas <stephanecandelas@gmail.com>
2019-09-27 23:27:28 +0100

    playing with the worldtimeapi.org

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
-const App = () => {
-	return <div>toto a ski but</div>;
-};
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			repos: []
+		};
+	}
+
+	async componentDidMount() {
+		// console.log("App componentDidMount");
+		// const result = await fetch("http://worldtimeapi.org/api/timezone");
+		// const json = await result.json();
+		// await console.log(json);
+		try {
+			const result = await axios.get(
+				"http://worldtimeapi.org/api/timezone/Australia/Brisbane"
+			);
+			console.log(result);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	render() {
+		return <div>toto a poil!</div>;
+	}
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
